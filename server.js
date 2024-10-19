b6952f2291842db851c4c2a9abf1c2d2aac27deb
Svetlana Dubkova <svetl@MacBook-Air-2.local>
2024-10-19 15:55:50 -0400

    changed port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ wss.on('connection', function connection(ws) {
   });
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 10000, () => {
   console.log(`Server running on port ${server.address().port}`);
 });
 
