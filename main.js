721d66451c1fb6f734272deabc6264f4dd726197
Svetlana Dubkova <svetl@MacBook-Air-2.local>
2024-10-21 19:57:30 -0400

    new

--- main.js
+++ main.js
@@ -128,7 +128,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentLoadingMessageIndex = (currentLoadingMessageIndex + 1) % loadingMessages.length;
         }, 3000);
 
-        const ws = new WebSocket('wss://zenafaiguidedmeditation.onrender.com');
+        const ws = new WebSocket('wss://humezenaf-svetlana-dubkovas-projects.vercel.app');
 
         ws.onopen = () => {
             console.log('Connected to server');
